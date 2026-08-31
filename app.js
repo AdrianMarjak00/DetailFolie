07b09f82bee4364dfb113f0784ed486e91cdd348
AdrianMarjak00 <adrianmarjak1@gmail.com>
2026-08-31 23:02:36 +0200

    ...

--- app.js
+++ app.js
@@ -245,5 +245,5 @@ document.querySelectorAll('.hub-card, .why-card, .price-card').forEach(card => {
   });
 });
 
-console.log('%c DetailFolie.com ⚡ ', 'background:#1d4ed8;color:#fff;font-size:14px;padding:5px 10px;border-radius:5px;font-weight:700');
+console.log('%c DetailFolie.com ⚡ ', 'background:#0055DD;color:#fff;font-size:14px;padding:5px 10px;border-radius:5px;font-weight:700');
 
